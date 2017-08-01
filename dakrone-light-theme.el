33723d070599c09c5311f5d570ebded284bab175
Lee Hinman <lee@writequit.org>
2017-08-01 13:32:28 -0600

    Add LGPL License header and LICENSE file
    Resolves #2

--- dakrone-light-theme.el
+++ dakrone-light-theme.el
@@ -7,6 +7,19 @@
 ;; Keywords: color themes faces
 ;; This file is not part of GNU Emacs.
 
+;;; License:
+;; This file is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU Lesser General Public License as published by the Free
+;; Software Foundation; either version 3, or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+;; A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+;; details.
+
+;; You should have received a copy of the GNU Lesser General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; A high-contrast light theme with greens and steely blues
 ;;
